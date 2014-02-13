bc2719abd15024ba30787c872c29b06e8be945dd
fatmah <fatmahedfi@gmail.com>
2014-02-13 11:23:28 +0100

    banner admin

--- src/STC/BannerBundle/Admin/BannerTypeAdmin.php
+++ src/STC/BannerBundle/Admin/BannerTypeAdmin.php
@@ -10,12 +10,24 @@ use Sonata\AdminBundle\Admin\Admin as BaseAdmin;
 
 class BannerTypeAdmin extends BaseAdmin
 {
-	
+	protected function configureFormFields(FormMapper $formMapper)
+	{
+		$formMapper
+		->add('name')
+		->add('position')
+		->add('default_media', 'sonata_media_type', array(
+				'provider' => 'sonata.media.provider.image',
+				'context'  => 'default'
+		))
+		->add('enabled');
+	}
 
 	protected function configureListFields(ListMapper $listMapper)
 	{
 		$listMapper
 		->add('name',NULL,array('label'=>'Banner Type Name'))
+		->add('defaut_media',null,array('label'=>'image','template'=>'STCBannerBundle:Admin:default_media_field.html.twig'))
+		->add('enabled')
 		;
 
 		$listMapper->add('_action', 'actions',array('actions' => array('edit' => array(), 'delete' => array(),)));
