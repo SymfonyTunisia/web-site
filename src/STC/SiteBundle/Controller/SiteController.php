1bb0e70bd1f7b42b7b689e29a562f643402697c2
Khaled Adala <kadala@ayaline.com>
2014-08-31 00:10:28 +0100

    Fixed Layout

--- src/STC/SiteBundle/Controller/SiteController.php
+++ src/STC/SiteBundle/Controller/SiteController.php
@@ -4,11 +4,41 @@ namespace STC\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Class SiteController
+ * @package STC\SiteBundle\Controller
+ */
 class SiteController extends Controller
 {
-	public function homeAction()
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function homeAction()
 	{
-		
 		return $this->render('STCSiteBundle:Site:home.html.twig');
 	}
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function whoWeAreAction()
+    {
+        return $this->render('STCSiteBundle:Site:whoWeAre.html.twig');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function copyrightAction()
+    {
+        return $this->render('STCSiteBundle:Site:copyright.html.twig');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function allRightsReservedAction()
+    {
+        return $this->render('STCSiteBundle:Site:allRightsReserved.html.twig');
+    }
 }
