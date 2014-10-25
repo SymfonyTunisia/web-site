b5102d47cf81cf7430b831d158a65a94ea5e3c16
Adala khaled <khaledadala@gmail.com>
2014-10-25 11:53:56 +0100

    Update app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
+umask(0000);
 require_once __DIR__ . '/../app/bootstrap.php.cache';
 require_once __DIR__ . '/../app/AppKernel.php';
 
