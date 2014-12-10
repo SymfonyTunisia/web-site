b69e160928c78ef98bdd494ebceca456bcd7bc7e
Khaled Adala <kadala@ayaline.com>
2014-12-10 22:29:36 +0100

    Fixed show speker

--- src/STC/SiteBundle/Resources/public/js/scripts.js
+++ src/STC/SiteBundle/Resources/public/js/scripts.js
@@ -3,8 +3,7 @@
 */
 $(window).load(function() {
     $('.flexslider').flexslider({
-        animation: "slide",
-        controlNav: "thumbnails"
+        animation: "slide"
     });
 });
 
@@ -66,41 +65,4 @@ jQuery(document).ready(function() {
     }); 
 });
 
-
-/*
-    Contact form
-*/
-jQuery(document).ready(function() {
-    $('.contact-form form').submit(function() {
-
-        $('.contact-form form .nameLabel').html('Name');
-        $('.contact-form form .emailLabel').html('Email');
-        $('.contact-form form .messageLabel').html('Message');
-
-        var postdata = $('.contact-form form').serialize();
-        $.ajax({
-            type: 'POST',
-            url: 'assets/sendmail.php',
-            data: postdata,
-            dataType: 'json',
-            success: function(json) {
-                if(json.nameMessage != '') {
-                    $('.contact-form form .nameLabel').append(' - <span class="violet" style="font-size: 13px; font-style: italic"> ' + json.nameMessage + '</span>');
-                }
-                if(json.emailMessage != '') {
-                    $('.contact-form form .emailLabel').append(' - <span class="violet" style="font-size: 13px; font-style: italic"> ' + json.emailMessage + '</span>');
-                }
-                if(json.messageMessage != '') {
-                    $('.contact-form form .messageLabel').append(' - <span class="violet" style="font-size: 13px; font-style: italic"> ' + json.messageMessage + '</span>');
-                }
-                if(json.nameMessage == '' && json.emailMessage == '' && json.messageMessage == '') {
-                    $('.contact-form form').fadeOut('fast', function() {
-                        $('.contact-form').append('<p><span class="violet">Thanks for contacting us!</span> We will get back to you very soon.</p>');
-                    });
-                }
-            }
-        });
-        return false;
-    });
-});
 
