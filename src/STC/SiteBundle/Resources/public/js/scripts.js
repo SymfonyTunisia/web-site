df72d92cbd13a18c8928a5aecb7d2ee71941141b
Khaled Adala <kadala@ayaline.com>
2014-12-10 22:19:55 +0100

    Fixed show speker

--- src/STC/SiteBundle/Resources/public/js/scripts.js
+++ src/STC/SiteBundle/Resources/public/js/scripts.js
@@ -8,35 +8,6 @@ $(window).load(function() {
     });
 });
 
-
-/*
-    Filterable portfolio
-*/
-jQuery(document).ready(function() {
-    $clientsHolder = $('ul.portfolio-img');
-    $clientsClone = $clientsHolder.clone(); 
- 
-    $('.filter-portfolio a').click(function(e) {
-        e.preventDefault();
-        $filterClass = $(this).attr('class');
- 
-        $('.filter-portfolio a').attr('id', '');
-        $(this).attr('id', 'active-imgs');
- 
-        if($filterClass == 'all'){
-            $filters = $clientsClone.find('li');
-        }
-        else {
-            $filters = $clientsClone.find('li[data-type~='+ $filterClass +']');
-        }
- 
-        $clientsHolder.quicksand($filters, {duration: 700}, function() {
-            $("a[rel^='prettyPhoto']").prettyPhoto({social_tools: false});
-        });
-    });
-});
-
-
 /*
     Pretty Photo
 */
